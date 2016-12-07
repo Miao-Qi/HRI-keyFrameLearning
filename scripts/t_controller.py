e18fa353e09fd471bd2b1ed15c67fd9a022ed846
Charles Luo <chunhengluo@outlook.com>
2016-12-06 21:16:19 -0600

    Add LED communication signals.

--- scripts/t_controller.py
+++ scripts/t_controller.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 
 import rospy
-from turtlesim.msg import Pose
-from std_msgs.msg import String
-from geometry_msgs.msg import Twist
 import math
+
+from turtlesim.msg     import Pose
+from std_msgs.msg      import String
+from std_msgs.msg      import ColorRGBA
+from geometry_msgs.msg import Twist
 
 #last_instruction = ""
 target_theta = 0
@@ -29,9 +31,16 @@ CurTwist = Twist()
 # For simulation ---------------------------------------------------
 # pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
 # For Sphero -------------------------------------------------------
-pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+pub      = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+pubColor = rospy.Publisher('set_color', ColorRGBA, queue_size=10) 
 
-
+def makeColor(r, g, b) : 
+  result = ColorRGBA() 
+  result.r = r 
+  result.g = g 
+  result.b = b 
+  result.a = 0.3
+  return result 
 
 # Use linear velocity instead of angular velocity to control direction
 def myCallback(data):
@@ -39,17 +48,43 @@ def myCallback(data):
     global CurTheta 
     global CurTwist 
     global pub
+
+    # Use LED light to show respond 
+    # default    white         (255, 255, 255) 
+    # go         green         (  0, 255,   0) 
+    # turn left  yellow        (255, 255,   0)    
+    # turn right blue          (  0,   0, 255)
+    # stop       red           (255,   0,   0)
+    # keep frame cyan          (  0, 255, 255)
     print (rospy.get_caller_id() + 'Current instruction: ' + data.data + "\n")
+
     if data.data == "go":
         CurTwist = createTwist(SPD * math.cos(CurTheta), SPD * math.sin(CurTheta))
+        pubColor.publish(makeColor(  0, 255,   0))
+        rospy.sleep(0.2)
+        pubColor.publish(makeColor(255, 255, 255))
     elif data.data == "turn right":
         CurTheta -= math.pi/2
         CurTwist = createTwist(SPD * math.cos(CurTheta), SPD * math.sin(CurTheta))
+        pubColor.publish(makeColor(  0,   0, 255))
+        rospy.sleep(0.2)
+        pubColor.publish(makeColor(255, 255, 255))
     elif data.data == "turn left":
         CurTheta += math.pi/2
         CurTwist = createTwist(SPD * math.cos(CurTheta), SPD * math.sin(CurTheta))
+        pubColor.publish(makeColor(255, 255,   0))
+        rospy.sleep(0.2)
+        pubColor.publish(makeColor(255, 255, 255))
     elif data.data == "stop":
         CurTwist = createTwist(0, 0)
+        pubColor.publish(makeColor(255,   0,   0))
+        rospy.sleep(0.2)
+        pubColor.publish(makeColor(255, 255, 255))
+    elif data.data == "keep frame": 
+        pubColor.publish(makeColor(  0, 255, 255))
+        rospy.sleep(0.2)
+        pubColor.publish(makeColor(255, 255, 255))
+
     print ("Publish new twist: ")
     print (CurTwist)
     print ("\n")
@@ -69,6 +104,7 @@ def turtle_controller():
     rospy.init_node('turtle_controller', anonymous=True)
     rospy.Subscriber('verbalInstruction', String, myCallback)
     rate = rospy.Rate(10)
+    pubColor.publish(makeColor(255, 255, 255))
     while not rospy.is_shutdown():
         pub.publish(CurTwist)
         rate.sleep()
